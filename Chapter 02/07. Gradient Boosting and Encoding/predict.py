e1880ec6c01daec76d2f0613248f9b0bcc6bd420
Kingsley-opoku <kingsley.opoku@mathmods.eu>
2022-04-20 16:55:48 +0200

    update predict file

--- Chapter 02/07. Gradient Boosting and Encoding/predict.py
+++ Chapter 02/07. Gradient Boosting and Encoding/predict.py
@@ -1,26 +1,30 @@
-from test import forest
-from train import ct
-#from sklearn.compose import ColumnTransformer
+from test import optimal_model 
 
-
-model=forest
-
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.compose import ColumnTransformer
 
 
 while True:
-
+    inputs=[]
     age = int(input("How old are you? \n"))
     child = int(input("How many children do you have? \n"))
     smoke = bool(input("Do you smoke? \n"))
     bmi=float(input('enter your BMI? \n'))
     sex=str(input('What is your sex? \n'))
     region=str(input('What region are you from? option: [northwest, southwest, northeast, southeast]\n'))
+    inputs.append([age, sex, bmi, child, smoke, region])
     '''
     Preprocess
     predict
     
     '''
-    
+    ct = ColumnTransformer([('ordinal', OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1), [1,4,5] ), 
+                                        ('numerical', StandardScaler(), [0, 2])], remainder='passthrough')
+
+    pred=ct.fit_transform(inputs)
+    model=optimal_model()
+    print(model.predict(pred))
+
+
 
     
-#print("You are too fucked up 1 milly")
